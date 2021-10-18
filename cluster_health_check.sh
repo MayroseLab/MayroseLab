210d16d251639c55237138717227f4b5ddd1656b
liorglic <liorglic@mail.tau.ac.il>
2021-10-18 16:50:21 +0300

    replace ssh with ping

--- cluster_health_check.sh
+++ cluster_health_check.sh
@@ -1,5 +1,10 @@
 for node in $(qmgr -c "p n @d" | grep -i itaym | awk '{print $3}' | sort | uniq)
 do
 	echo $node
-	ssh $node "echo OK"
+	pingRes=$(ping $node -c1 | grep error)
+	if [ -z "$pingRes" ]; then
+		echo "OK"
+	else
+		echo "Can't reach node"
+	fi
 done
